63e5e579c202549da963853adcda570601300161
sandworm47 <owen.cleland@gmail.com>
2023-04-03 11:33:25 -0600

    redis stedi stuff

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const https = require ('https');
 const fs = require ('fs');
 
 const redisClient = redis.createClient({
-    url:'redis://127.0.0.1:6379'
+    url:'redis://default:Trinitrotoluene#479944@redis-stedi-owen:6379'
 });
 
 const {v4: uuidv4} = require('uuid');//universally unique identifier
